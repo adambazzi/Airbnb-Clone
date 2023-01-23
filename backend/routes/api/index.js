51a3de9e79b1a80670b85ccee3d43b874e887deb
Adam Bazzi <adambazzi14@gmail.com>
2023-01-23 13:51:40 -0600

    added some test paths

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,10 +5,17 @@ const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
 
+router.get('/test', function (req,res) {
+    res.json({ requestBody: req.body });
+});
 
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
+
+router.post('/test', function (req,res) {
+    res.json({ requestBody: req.body });
+});
 
 // router.post('/test', function(req, res) {
 //     res.json({ requestBody: req.body });
