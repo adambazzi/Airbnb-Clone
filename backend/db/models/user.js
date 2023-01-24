dd86e086c7505d74e2e670e51c25acff95a24cd6
Adam Bazzi <adambazzi14@gmail.com>
2023-01-24 12:55:49 -0600

    users signup, login, get token requests verified

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -6,8 +6,8 @@ const bcrypt = require('bcryptjs');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     toSafeObject() {
-      const { id, username, email } = this; // context will be the User instance
-      return { id, username, email };
+      const { id, username, email, firstName, lastName } = this; // context will be the User instance
+      return { id, username, email, firstName, lastName };
     }
 
     validatePassword(password) {
@@ -34,7 +34,7 @@ module.exports = (sequelize, DataTypes) => {
       }
     }
 
-    static async signup({ username, email, password }) {
+    static async signup({ username, email, password, firstName, lastName }) {
       const hashedPassword = bcrypt.hashSync(password);
       const user = await User.create({
         username,
