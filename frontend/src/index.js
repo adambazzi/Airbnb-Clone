d53b3949601fe4eeeba59fd358c6e46ea2f1682c
Adam Bazzi <adambazzi14@gmail.com>
2023-04-16 13:29:15 -0500

    updated frontend index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import 'react-datepicker/dist/react-datepicker.css';
-
-
 import './index.css';
-
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { ModalProvider, Modal } from './context/Modal';
 import App from './App';
-
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from "./store/csrf";
 import * as sessionActions from './store/session';
@@ -23,9 +19,6 @@ if (process.env.NODE_ENV !== "production") {
   window.sessionActions = sessionActions;
 }
 
-// Wrap the application with the Modal provider and render the Modal component
-// after the App component so that all the Modal content will be layered as
-// HTML elements on top of the all the other HTML elements:
 function Root() {
   return (
     <ModalProvider>
@@ -39,9 +32,5 @@ function Root() {
   );
 }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Root />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root');
+ReactDOM.createRoot(rootElement).render(<React.StrictMode><Root /></React.StrictMode>);
